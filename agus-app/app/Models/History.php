52402fbc6e60c2e42f704924643b7e0a588ed036
capillatussss <a.wannnn@gmail.com>
2023-08-15 11:00:11 +0700

    commit message

--- agus-app/app/Models/History.php
+++ agus-app/app/Models/History.php
@@ -5,9 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Submission extends Model
+class History extends Model
 {
     use HasFactory;
 
@@ -16,13 +15,13 @@ class Submission extends Model
       return $this->belongsTo(User::class);
     }
 
+    public function submission(): BelongsTo
+    {
+      return $this->belongsTo(Submission::class);
+    }
+
     public function status(): BelongsTo
     {
       return $this->belongsTo(Status::class);
-    }
-
-    public function histories(): HasMany
-    {
-      return $this->hasMany(History::class);
     }
 }
